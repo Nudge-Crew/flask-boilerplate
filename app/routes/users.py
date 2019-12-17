38f99a3d2773fff1638b5c41c08d4a9cd379f30d
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-12-17 10:32:59 +0100

    more generic user route variables

--- app/routes/users.py
+++ app/routes/users.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 
 from app.resources import user_resource
 
-USER_PREFIX = "users"
+PREFIX = "users"
 
-USER_BLUEPRINT = Blueprint(USER_PREFIX, __name__)
+USER_BLUEPRINT = Blueprint(PREFIX, __name__)
 
 
 @USER_BLUEPRINT.route('/', methods=['GET'])
